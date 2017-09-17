3a11078374efdc1bc14c8ec114118a2cad3270f3
vtzika <varvara.tzika@live.com>
2017-09-17 17:18:40 +0200

    typos fixed

--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@ var book_url = 'https://www.bol.com/nl/p/continuous-delivery/1001004007694286/'
 var add_to_basket_btn = element(by.id("1001004007694286"));
 var checkout_btn = element(by.linkText('bestellen'));
 var basket_url = 'https://www.bol.com/nl/order/basket.html';
-var totals_table_rows = element.all(by.class('totals__table tr'));
+var totals_table_rows = element.all(by.css('.totals__table tr'));
 var totalPriceRowIndex = 2;
 var deliveryFeeRowIndex = 1;
 
@@ -21,20 +21,20 @@ describe('When an order costs more than 20 euros', function() {
 		continueToBasket();
 
 		var currentUrl = browser.getCurrentUrl();
-    	expect(currentUrl.toEqual(basket_url));
+    	expect(currentUrl).toEqual(basket_url);
 
-		var totalPrice = getTheTotalPrice();
+		var totalPrice = getTotalOrderPrice();
 		expect(totalPrice).toBeGreaterThan(40);
 
 		var deliveryFee = getDeliveryFee();
-    	expect(deliveryFee.toEqual("Free"));
+    	expect(deliveryFee).toEqual("Free");
 	});
 });
 
 
 function addBookToshoppingList(){
 	browser.get(book_url);
-	add_to_basket.click().then(function(){
+	add_to_basket_btn.click().then(function(){
 		});
 }
 
