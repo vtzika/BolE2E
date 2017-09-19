f53e1b67854ef529353688dc4f5e46c9afc8e82f
vtzika <varvara.tzika@live.com>
2017-09-19 10:59:31 +0200

    Typos and after each test comment is added

--- spec.js
+++ spec.js
@@ -1,15 +1,15 @@
 // spec.js
-var moreThan20eurosBookUrl = 'https://www.bol.com/nl/p/continuous-delivery/1001004007694286/';
-var lessThan20eurosBookUrl = 'https://www.bol.com/nl/p/the-phoenix-project/9200000027983023/';
-var 20eurosBookUrl = 'https://www.bol.com/nl/p/data-analyseren-en-programmeren-met-r/9200000077965963/';
-var addToBasketMoreThan20eurosBtn = element(by.id("1001004007694286"));
-var addToBasketLessThan20eurosBtn = element(by.id("9200000027983023"));
-var addToBasket20eurosBtn = element(by.id("9200000077965963"));
+var moreThanTwentyEurosBookUrl = 'https://www.bol.com/nl/p/continuous-delivery/1001004007694286/';
+var lessThanTwentyEurosBookUrl = 'https://www.bol.com/nl/p/the-phoenix-project/9200000027983023/';
+var twentyEurosBookUrl = 'https://www.bol.com/nl/p/data-analyseren-en-programmeren-met-r/9200000077965963/';
+var addToBasketMoreThanTwentyEurosBtn = element(by.id("1001004007694286"));
+var addToBasketLessThanTwentyEurosBtn = element(by.id("9200000027983023"));
+var addToBasketTwentyEurosBtn = element(by.id("9200000077965963"));
 var checkoutBtn = element(by.linkText('bestellen'));
 var basketUrl = 'https://www.bol.com/nl/order/basket.html';
-var totalsTableRows = element.all(by.css('.totals__table tr'));
-var totalPriceRowIndex = 2;
-var deliveryFeeRowIndex = 1;
+var totalsTableRows = element.all(by.css('.totals__table tr td'));
+var totalPriceRowIndex = 3;
+var deliveryFeeRowIndex = 2;
 
 
 beforeEach(function() {
@@ -17,12 +17,17 @@ beforeEach(function() {
 	browser.manage().window().maximize();
 });
 
+afterEach(function() {
+	//TODO clean basket
+});
+
+
 
 describe('When an order costs more than 20 euros', function() {
 	it('should not charge for delivery', function() {
 
-		browser.get(moreThan20eurosBookUrl);
-		addBookToshoppingList(addToBasketMoreThan20eurosBtn);
+		browser.get(moreThanTwentyEurosBookUrl);
+		addBookToshoppingList(addToBasketMoreThanTwentyEurosBtn);
 		continueToBasket();
 
 		var currentUrl = browser.getCurrentUrl();
@@ -41,15 +46,15 @@ describe('When an order costs more than 20 euros', function() {
 describe('When an order costs less than 20 euros', function() {
 	it('should charge for delivery', function() {
 
-		browser.get(moreThan20eurosBookUrl);
-		addBookToshoppingList(addToBasketLessThan20eurosBtn);
+		browser.get(lessThanTwentyEurosBookUrl);
+		addBookToshoppingList(addToBasketLessThanTwentyEurosBtn);
 		continueToBasket();
 
 		var currentUrl = browser.getCurrentUrl();
     	expect(currentUrl).toEqual(basketUrl);
 
 		var totalPrice = getTotalOrderPrice();
-		expect(totalPrice).toBeLessThan(20);
+		expect(totalPrice).toEqual('Gratis');
 
 		var deliveryFee = getDeliveryFee();
     	expect(deliveryFee).toBeGreaterThan(0);
@@ -60,18 +65,18 @@ describe('When an order costs less than 20 euros', function() {
 describe('When an order costs 20 euros', function() {
 	it('should not charge for delivery', function() {
 
-		browser.get(20eurosBookUrl);
-		addBookToshoppingList(addToBasket20eurosBtn);
+		browser.get(twentyEurosBookUrl);
+		addBookToshoppingList(addToBasketTwentyEurosBtn);
 		continueToBasket();
 
 		var currentUrl = browser.getCurrentUrl();
     	expect(currentUrl).toEqual(basketUrl);
 
 		var totalPrice = getTotalOrderPrice();
-		expect(totalPrice).toEqual(20);
+		expect(totalPrice).toEqual('Gratis');
 
 		var deliveryFee = getDeliveryFee();
-    	expect(deliveryFee).toEqual("Free");
+    	expect(deliveryFee).toBeGreaterThan(0);
 	});
 });
 
@@ -85,11 +90,11 @@ function continueToBasket(){
 }
 
 function getTotalOrderPrice(){
-	var totalOrderPrice = totalsTableRows.get(totalPriceRowIndex).element(by.css('td')).getText();
+	var totalOrderPrice = totalsTableRows.get(totalPriceRowIndex).getText();
 	return totalOrderPrice;
 }
 
 function getDeliveryFee(){
-	var deliveryFee = totalsTableRows.get(deliveryFeeRowIndex).element(by.css('td')).getText();
+	var deliveryFee = totalsTableRows.get(deliveryFeeRowIndex).getText();
 	return deliveryFee;
 }
