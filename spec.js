7371c099a7efdbda15143b726e9e14574dbaef1a
vtzika <varvara.tzika@live.com>
2017-09-19 10:27:08 +0200

    Test for 20euros free delivery is added

--- spec.js
+++ spec.js
@@ -1,8 +1,10 @@
 // spec.js
 var moreThan20eurosBookUrl = 'https://www.bol.com/nl/p/continuous-delivery/1001004007694286/';
 var lessThan20eurosBookUrl = 'https://www.bol.com/nl/p/the-phoenix-project/9200000027983023/';
+var 20eurosBookUrl = 'https://www.bol.com/nl/p/data-analyseren-en-programmeren-met-r/9200000077965963/';
 var addToBasketMoreThan20eurosBtn = element(by.id("1001004007694286"));
 var addToBasketLessThan20eurosBtn = element(by.id("9200000027983023"));
+var addToBasket20eurosBtn = element(by.id("9200000077965963"));
 var checkoutBtn = element(by.linkText('bestellen'));
 var basketUrl = 'https://www.bol.com/nl/order/basket.html';
 var totalsTableRows = element.all(by.css('.totals__table tr'));
@@ -16,8 +18,8 @@ beforeEach(function() {
 });
 
 
-describe('When an order costs less than 20 euros', function() {
-	it('should have a delivery fee', function() {
+describe('When an order costs more than 20 euros', function() {
+	it('should not charge for delivery', function() {
 
 		browser.get(moreThan20eurosBookUrl);
 		addBookToshoppingList(addToBasketMoreThan20eurosBtn);
@@ -36,8 +38,8 @@ describe('When an order costs less than 20 euros', function() {
 
 
 
-describe('When an order costs more than 20 euros', function() {
-	it('should provide free delivery', function() {
+describe('When an order costs less than 20 euros', function() {
+	it('should charge for delivery', function() {
 
 		browser.get(moreThan20eurosBookUrl);
 		addBookToshoppingList(addToBasketLessThan20eurosBtn);
@@ -47,10 +49,29 @@ describe('When an order costs more than 20 euros', function() {
     	expect(currentUrl).toEqual(basketUrl);
 
 		var totalPrice = getTotalOrderPrice();
-		expect(totalPrice).toBeGreaterThan(40);
+		expect(totalPrice).toBeLessThan(20);
 
 		var deliveryFee = getDeliveryFee();
     	expect(deliveryFee).toBeGreaterThan(0);
+	});
+});
+
+
+describe('When an order costs 20 euros', function() {
+	it('should not charge for delivery', function() {
+
+		browser.get(20eurosBookUrl);
+		addBookToshoppingList(addToBasket20eurosBtn);
+		continueToBasket();
+
+		var currentUrl = browser.getCurrentUrl();
+    	expect(currentUrl).toEqual(basketUrl);
+
+		var totalPrice = getTotalOrderPrice();
+		expect(totalPrice).toEqual(20);
+
+		var deliveryFee = getDeliveryFee();
+    	expect(deliveryFee).toEqual("Free");
 	});
 });
 
